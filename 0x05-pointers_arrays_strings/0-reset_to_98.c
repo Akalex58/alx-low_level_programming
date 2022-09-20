e4b09822bc5ef89b2165833c14123ee4181dc020
Eyo <akalex.akalex@gmail.com>
2022-09-20 15:33:43 -0700

    reset to 98 task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,8 +5,14 @@
 *
 *@n: update the value points to to 98
 */
-void() reset_to_98(int *n);
+int mian(void)
 {
-	*n = 98;
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	
+	n = 98 /*reset to 98 the value of n*/
+	printf("n=%d\n", n);
 	return;
 }
