b0801a02f28ec3e8d629b9df8b5dc9b5d6569bb2
Eyo <akalex.akalex@gmail.com>
2022-09-23 23:50:25 -0700

    strcat task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,41 +1,28 @@
 #include "main.h"
 
-int count_string(char *str);
-
 /**
- * _strcat - a copy of strcat, concantinate two strings.
- * @dest: pointer to the destination string.
- * @src: pointer to the source string.
- * Return: updated pointer to the destination string [dest].
+ * _strcat - concats two arrays
+ *
+ * @dest: destination of concat
+ * @src: source array to concat
+ *
+ * Return: char value
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int src_lenght, dest_lenght, i;
+	int i;
+	int j;
 
-	src_lenght = count_string(src);
-	dest_lenght = count_string(dest);
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
 
-	for (i = 0; i < src_lenght; i++)
-		dest[i + dest_lenght] = src[i];
-	dest[src_lenght + dest_lenght + 1] = '\0';
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
 
 	return (dest);
-}
 
-/**
- * count_string - Count  number of char in a string excluding '\0'
- * Return: total string lenght
- * @str: being the string to count
- */
-
-int count_string(char *str)
-{
-	int count;
-
-	count = 0;
-	while (str[count] != '\0')
-		count++;
-
-	return (count);
 }
